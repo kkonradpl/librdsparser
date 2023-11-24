6d2ca4e5684fb31816c1219bbac1c1f47832c3a3
Konrad Kosmatka <kkonrad92@gmail.com>
2023-11-24 02:09:03 +0100

    Add missing assert.h

---
+++ test/asserts.h
@@ -0,0 +1,25 @@
+#ifndef LIBRDS_TESTS_ASSERTS_H
+#define LIBRDS_TESTS_ASSERTS_H
+#include <wchar.h>
+
+#ifdef LIBRDS_DISABLE_UNICODE
+#define assert_rds_string_equal(current, expected) \
+    { \
+        size_t length = wcslen(expected); \
+        librds_string_char_t *ascii = calloc(length + 1, 1); \
+        for (size_t i = 0; i < length; i++) \
+        { \
+            ascii[i] = ((expected[i] >= 0x7F) ? ' ' : (char)expected[i]); \
+        } \
+        assert_string_equal((const char*)current, (const char*)ascii); \
+        free(ascii); \
+    }
+#else
+#define assert_rds_string_equal(current, expected) \
+    if (wcscmp(current, expected) != 0) \
+    { \
+        fail_msg("\"%ls\" != \"%ls\"", current, expected); \
+    }
+#endif
+
+#endif
